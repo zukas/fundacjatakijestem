948b4b019b757c4787cf0cda747374ac00243ef7
zukas <julius.zukauskas@gmx.com>
2013-12-22 15:09:16 +0000

    dynamic content workspace imp

--- public/script/index.js
+++ public/script/index.js
@@ -1,55 +1,56 @@
 $( document ).ready(function() {
+
+	var _calcHeight = function (){
+			var _height = $("body div.content .data").height();
+			var _calc = $("body").height() - 220;
+			_calc = _height>_calc?_height:_calc;
+			$("body div.content").css("height", _calc);		
+		},
+		_loader = function (data) {
+			$("body div.content .data").html(data);
+			_calcHeight()
+		};
+
 	$.ajax({
 		type: "GET",
 		url: "public/html/__home.html"
-	}).done(function( data ) {
-		$("body div.content").html(data);
-	});
+	}).done(_loader);
 
 	$(".navigation .nav.home").click(function(){
 		$.ajax({
 			type: "GET",
 			url: "public/html/__home.html"
-		}).done(function( data ) {
-			$("body div.content").html(data);
-		});
+		}).done(_loader);
 	});
 
 	$(".navigation .nav.about").click(function(){
 		$.ajax({
 			type: "GET",
 			url: "public/html/__about.html"
-		}).done(function( data ) {
-			$("body div.content").html(data);
-		});
+		}).done(_loader);
 	});
 
 	$(".navigation .nav.gallery").click(function(){
 		$.ajax({
 			type: "GET",
 			url: "public/html/__gallery.html"
-		}).done(function( data ) {
-			$("body div.content").html(data);
-		});
+		}).done(_loader);
 	});
 
 	$(".navigation .nav.events").click(function(){
 		$.ajax({
 			type: "GET",
 			url: "public/html/__events.html"
-		}).done(function( data ) {
-			$("body div.content").html(data);
-		});
+		}).done(_loader);
 	});
 
 	$(".navigation .nav.contact").click(function(){
 		$.ajax({
 			type: "GET",
 			url: "public/html/__contact.html"
-		}).done(function( data ) {
-			$("body div.content").html(data);
-		});
+		}).done(_loader);
 	});
 
-
+	$( window ).resize(_calcHeight);
 });
+
