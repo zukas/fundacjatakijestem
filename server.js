b0c1da82663f32a1dc652c7b714d4025def3c952
zukas <julius.zukauskas@gmx.com>
2013-12-23 21:27:51 +0000

    performance optimisation

--- server.js
+++ server.js
@@ -5,39 +5,48 @@ var http = require('http'),
 // Configure our HTTP server to respond with Hello World to all requests.
 var server = http.createServer(function (request, response) {
 
-	var __writeHandler = function (err, data) {
+	var _cache = {},
+		__writeHandler = function (err, data) {
 		
-		if (err) {
-			console.log("Error reading file");
-			console.log(err);
-			response.writeHead(404);
-		} else {
-			var _type = request.url.substring(request.url.lastIndexOf(".")+1),
-				_mime = {
-					"html" : "text/html",
-					"css" : "text/css",
-					"js" : "application/javascript",
-					"png" : "image/png",
-					"jpg" : " image/jpeg",
-					"jpeg" : " image/jpeg"
+			if (err) {
+				console.log("Error reading file");
+				console.log(err);
+				response.writeHead(404);
+			} else {
+				if(!_cache[request.url]){
+					_cache[request.url] = data;
 				}
+				
+				var _type = request.url.substring(request.url.lastIndexOf(".")+1),
+					_mime = {
+						"html" : "text/html",
+						"css" : "text/css",
+						"js" : "application/javascript",
+						"png" : "image/png",
+						"jpg" : " image/jpeg",
+						"jpeg" : " image/jpeg"
+					}
 
-			response.writeHead(200, {"Content-Type": _mime[_type]});
-			response.write(data);
+				response.writeHead(200, {"Content-Type": _mime[_type]});
+				response.write(data);
+			}
+			
+	  		response.end();
 		}
-		
-  		response.end();
-	}
 
 	if(request.url === "/") {
 		fs.readFile(__dirname + "/public/html/index.html", __writeHandler);
 	} else {
 		request.url = __dirname + request.url;
-		if(fs.existsSync(request.url)) {
-			fs.readFile(request.url, __writeHandler);
-		}else{
-			response.writeHead(302, { "Location": "/"});
-			response.end();
+		if(_cache[request.url]){
+			__writeHandler(null, _cache[request.url]);
+		} else {
+			if(fs.existsSync(request.url)) {
+				fs.readFile(request.url, __writeHandler);
+			}else{
+				response.writeHead(302, { "Location": "/"});
+				response.end();
+			}
 		}
 	}
 
