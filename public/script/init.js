89b498d1fb3fb90ee83a1512edb2b859e083b5cd
zukas <julius.zukauskas@gmx.com>
2014-12-02 18:41:55 +0100

    changed text

--- public/script/init.js
+++ public/script/init.js
@@ -3,19 +3,6 @@ App.isIE = function () {
     var ua = window.navigator.userAgent;
     var msie = ua.indexOf('MSIE ');
     var trident = ua.indexOf('Trident/');
-
-    // if (msie > 0) {
-    //     // IE 10 or older => return version number
-    //     return parseInt(ua.substring(msie + 5, ua.indexOf('.', msie)), 10);
-    // }
-
-    // if (trident > 0) {
-    //     // IE 11 (or newer) => return version number
-    //     var rv = ua.indexOf('rv:');
-    //     return parseInt(ua.substring(rv + 3, ua.indexOf('.', rv)), 10);
-    // }
-
-    // other browser
     return msie > 0 || trident > 0;
 }
 
