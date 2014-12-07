299b8619eef6da15581500aaa43ea9bfd9b8c1ec
zukas <julius.zukauskas@gmx.com>
2014-12-07 15:35:49 +0100

    season update

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,26 @@
 var engine = require('../engines/engine');
 
+function getSeason() {
+	var m = (new Date()).getMonth() + 1;
+	var res = '';
+	switch(m) 
+	{
+		case 1: 
+		case 2: { res = "winter"; break; }
+		case 3:
+		case 4:
+		case 5: { res = "spring"; break; }
+		case 6:
+		case 7:
+		case 8: { res = "summer"; break; }
+		case 9:
+		case 10:
+		case 11: { res = "autum"; break; }
+		case 12: { res = "winter"; break; }
+	}
+	return res;
+}
+
 exports.index = function(req, res){
 	var page = ({
 		about : 'about.html',
@@ -8,7 +29,7 @@ exports.index = function(req, res){
 		contact : 'contact.html'
 	})[req.params.page] || 'home.html';
 
-	res.render(page);
+	res.render(page, { season : getSeason() });
 };
 
 exports.load = function(req, res) {
